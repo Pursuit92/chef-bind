be27357656ae895156c068c1fe5a006f23347386
Josh Chase <jcjoshuachase@gmail.com>
2014-07-23 14:03:23 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'bind'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Josh Chase'
+maintainer_email 'jcjoshuachase@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures bind'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
